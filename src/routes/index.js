9b1c973326f2e5c5ad471a5505aebde28df49f27
Jobe Diego Dylbas dos Santos <jobe.dylbas@inf.ufrgs.br>
2022-04-11 15:34:23 -0300

    Resolve bugs e adiciona features de tipos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,15 @@ const fetch = require('node-fetch');
 const router = express.Router()
 
 router.get('/', async function (req, res, next) {
-  res.render('index')
+  res.render('index', { breadcrumbs: [] })
 })
 
 router.get('/about', async function (req, res, next) {
-  res.render('about')
+  res.render('about', { breadcrumbs: ["Sobre"] })
 })
 
 router.get('/generations', async function (req, res, next) {
-  res.render('generations')
+  res.render('generations', { breadcrumbs: ["Gerações"] })
 })
 
 router.get('/generation', async function (req, res, next) {
@@ -29,15 +29,28 @@ router.get('/generation', async function (req, res, next) {
     if(a.name > b.name) { return 1; }
     return 0;
   })
-
-  res.render('generation', { pokemons: pokemons, id: req.query.id } )
+  let lastBreadcrumb = "Geração "+req.query.id
+  res.render('generation', { pokemons: pokemons, id: req.query.id, breadcrumbs: [{ name: 'Gerações', path: '/generations' }, lastBreadcrumb]} )
 })
 
-router.get('/pokemon', async function (req, res, next) {
+router.get('/pokemons/pokemon', async function (req, res, next) {
   let response = await fetch('https://pokeapi.co/api/v2/pokemon/'+req.query.id)
   let data = await response.json()
+  let dataName = data.name.charAt(0).toUpperCase() + data.name.slice(1)
+  res.render('pokemon', { pokemon: data, breadcrumbs: [{name: "Pokemons", path: '/pokemons'}, dataName] })
+})
 
-  res.render('pokemon', { pokemon: data })
+router.get('/pokemons', async function (req, res, next) {
+  let response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset='+req.query.offset+'&limit='+10)
+  let data = await response.json()
+
+  let pokemons = await Promise.all(data.results.map(async (item) => {
+    let response = await fetch(item.url)
+  
+    return await response.json()
+  }))
+
+  res.render('pokemons', { pokemons: pokemons, offset: parseInt(req.query.offset), breadcrumbs: ["Pokemons"] })
 })
 
 router.get('/types', async function (req, res, next) {
@@ -47,5 +60,14 @@ router.get('/types', async function (req, res, next) {
   res.render('types', { response: data })
 })
 
+router.get('/types/type', async function (req, res, next) {
+  let response = await fetch('https://pokeapi.co/api/v2/type/'+req.query.id)
+
+  let data = await response.json()
+  let dataName = data.name.charAt(0).toUpperCase() + data.name.slice(1)
+  let path = [{name: "Tipos", path: "types"}, dataName]
+  
+  res.render('type', { response: data, breadcrumbs: path })
+})
 
 module.exports = router
